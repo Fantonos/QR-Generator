53ff807fabc9ec25c8f492c996a4b3f100b9cc31
Fantonos <galaxxy@sudomail.com>
2025-01-19 23:28:53 -0500

    Optimized 1

--- main.py
+++ main.py
@@ -7,10 +7,12 @@ start_dir = os.getcwd()
 
 
 def main():
+    # create the QR Codes folder if it doesn't exist
     today_str = str(date.today())
     for folder in ["QR Codes", f"{today_str} Codes"]:
         create_select_dir(folder)
 
+    # get the name of the QR Code
     while True :
         qrcode_name = input("Enter the name of the QR Code: ")
         if check_name_forbidden_chars(qrcode_name):
@@ -22,6 +24,7 @@ def main():
     
     data = input("Enter the data you want to encode: ")
     
+    # create the QR Code
     try:
         img = qrcode.make(data)
         img.save(check_file_exists(qrcode_name))
@@ -30,18 +33,17 @@ def main():
         os.chdir(start_dir)
         main()
         return
-
-    
     print("QR Code created successfully!")
 
 
 def create_select_dir(directory_name):
+    # Create the directory if it doesn't exist
     os.makedirs(directory_name, exist_ok=True)
     os.chdir(directory_name)
 
 
 def confirm_name(user_input, qr_name):
-    #Strip removes leading/trailing whitespaces 
+    # Strip removes leading/trailing whitespaces 
     response = user_input.strip().upper()
     
     if response in ("Y", "YES"):
@@ -51,6 +53,7 @@ def confirm_name(user_input, qr_name):
 
 
 def check_file_exists(qr_name):
+    # Check if file exists, if it does, add a number to the end of the name
     global name_count
 
     while True:
@@ -58,14 +61,15 @@ def check_file_exists(qr_name):
             file_name = f"{qr_name}.png"
         else:
             file_name = f"{qr_name}({name_count}).png"
+
         if not os.path.exists(file_name):
             return file_name
         name_count += 1
  
         
 def check_name_forbidden_chars(name):
-    forbidden_chars = {'\\', '/', ':', '*', '?', '"', "'", '<', '>', '|'}
     # Return True if length is too long OR if there's any forbidden character
+    forbidden_chars = {'\\', '/', ':', '*', '?', '"', "'", '<', '>', '|'}
     return len(name) > 230 or bool(set(name) & forbidden_chars)
 
 
